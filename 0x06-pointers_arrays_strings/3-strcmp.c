9d21ee08a6b72af543007d12d860e14ac627bc58
Keobakile <eddisonk@gmail.com>
2022-09-23 14:21:55 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,9 @@
 * @s1: first string
 * @s2: second string
 * Return:
-*		return zero if s1 == s2
-*		return negative number if s1 < s2
-*		return positive number if s1 > s2
+*		returns zero if s1 == s2
+*		returns negative number if s1 < s2
+*		returns positive number if s1 > s2
 */
 
 int _strcmp(char *s1, char *s2)
@@ -34,7 +34,7 @@ int _strcmp(char *s1, char *s2)
 			break;
 		}
 		else
-			i++
+			i++;
 	}
 	return (diff);
 }
